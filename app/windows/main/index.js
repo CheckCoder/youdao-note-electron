92f577ef207f7c532bffa910fa8977a1f3f19a6c
checkcai <checkcai@wesure.cn>
2021-01-21 22:04:56 +0800

    feat: 优化头像框 & 顶部区域

--- app/windows/main/index.js
+++ app/windows/main/index.js
@@ -3,45 +3,82 @@
  */
 function handleIframeContainerOnload () {
     console.log('handle iframe-container onload');
-    let iframeContainer = document.getElementById('iframe-container');
-    let { href } = iframeContainer.contentWindow.location;
-    console.log(href);
-    if (href.includes('note.youdao.com/signIn')) {
-        // 登录页
-        console.log('进入 signin 页');
-        getIframeContainerDocument().getElementsByClassName('hd')[0].style.display = "none";
-    } else if (href.includes('note.youdao.com/web')) {
-        // 首页
-        console.log('进入 web 页');
-        // 设置顶部区域可见性
-        getIframeContainerDocument().getElementsByClassName('top-banner')[0].style.visibility = "hidden";
-        // 设置头像区域可见性
-        getIframeContainerDocument().getElementsByClassName('top-right')[0].style.visibility = "visible";
-        getIframeContainerDocument().getElementsByClassName('info-more')[0].style.visibility = "visible";
-        // 移除不需要的元素
-        getIframeContainerDocument().getElementsByClassName('to-website')[0].style.display = "none";
-        getIframeContainerDocument().getElementsByClassName('top-line')[0].style.display = "none";
-        getIframeContainerDocument().getElementsByClassName('update-vip')[0].style.display = "none";
-        getIframeContainerDocument().getElementsByClassName('top-line')[1].style.display = "none";
-        getIframeContainerDocument().getElementsByClassName('top-btns')[0].style.display = "none";
-        // 设置主区域
-        getIframeContainerDocument().getElementsByClassName('main-container')[0].style.top = "0";
-        // 设置头像框
-        let avatarContainerElement = document.createElement('div');
-        avatarContainerElement.style.cssText = 'margin:0 auto; width:100%';
-        avatarContainerElement.appendChild(getIframeContainerDocument().getElementsByClassName('top-right')[0]);
-        let createDocumentElement = getIframeContainerDocument().getElementsByClassName('create')[0];
-        getIframeContainerDocument().getElementsByClassName('hd')[0].insertBefore(avatarContainerElement, createDocumentElement);
-        getIframeContainerDocument().getElementsByClassName('own-photo')[0].style.cssText = 'width:38px; height:38px; border-radius:19px';
-        getIframeContainerDocument().getElementsByClassName('info-more')[0].style.cssText = 'z-index:1; font-size:14px; line-height:50px';
-        // getIframeContainerDocument().getElementsByClassName('hd')[0].style.paddingTop = "50px";
-        // getIframeContainerDocument().getElementsByClassName('sidebar-content')[0].style.top = "111px";
+    setTimeout(()=>{
+        let iframeContainer = document.getElementById('iframe-container');
+        let { href } = iframeContainer.contentWindow.location;
+        console.log(href);
+        if (href.includes('note.youdao.com/signIn')) {
+            // 登录页
+            console.log('进入 signin 页');
+            getIframeContainerDocument().getElementsByClassName('hd')[0].style.display = "none";
+        } else if (href.includes('note.youdao.com/web')) {
+            // 首页
+            console.log('进入 web 页');
+            // 设置顶部区域可见性
+            getIframeContainerDocument().getElementsByClassName('top-banner')[0].style.display = "none";
+            // 设置主区域上移
+            getIframeContainerDocument().getElementsByClassName('main-container')[0].style.top = "0";
+            // 移除不需要的元素
+            let originalAvatarElement = getIframeContainerDocument().getElementsByClassName('top-right')[0];
+            originalAvatarElement.removeChild(originalAvatarElement.children[2]);
+            originalAvatarElement.removeChild(originalAvatarElement.children[2]);
+            originalAvatarElement.removeChild(originalAvatarElement.children[2]);
+            originalAvatarElement.removeChild(originalAvatarElement.children[2]);
+            // 设置头像框
+            setStyleFromComputedStyle(originalAvatarElement);
+            originalAvatarElement.style.position = "relative";
+            originalAvatarElement.style.right = "unset";
+            // getIframeContainerDocument().getElementsByClassName('own-photo')[0].style.width = '50px';
+            // getIframeContainerDocument().getElementsByClassName('own-photo')[0].style.height = '50px';
+            getIframeContainerDocument().getElementsByClassName('own-photo')[0].style.borderRadius = '19px';
+            getIframeContainerDocument().getElementsByClassName('info-more')[0].style.position = 'absolute';
+            getIframeContainerDocument().getElementsByClassName('info-more')[0].style.left = '0';
+            getIframeContainerDocument().getElementsByClassName('info-more')[0].style.width = '50px';
+            
+            // 删除部分菜单子项
+            let menuElement = getIframeContainerDocument().getElementsByClassName('widget-menu')[0];
+            menuElement.removeChild(menuElement.children[2]);
+            menuElement.removeChild(menuElement.children[2]);
+            menuElement.removeChild(menuElement.children[2]);
+            menuElement.removeChild(menuElement.children[2]);
+            menuElement.removeChild(menuElement.children[2]);
+            menuElement.removeChild(menuElement.children[2]);
+            // 还原菜单子项 hover 颜色
+            getIframeContainerDocument().getElementsByClassName('widget-menu-item')[0].style.background = ""
+            getIframeContainerDocument().getElementsByClassName('widget-menu-item')[2].style.background = ""
+            // 放置头像框
+            let avatarContainerElement = document.createElement('div');
+            avatarContainerElement.style.cssText = 'width:100%; display:flex; flex-direction: row; justify-content: center;top: 8px;position: relative;';
+            avatarContainerElement.appendChild(getIframeContainerDocument().getElementsByClassName('top-right')[0]);
+            let createDocumentElement = getIframeContainerDocument().getElementsByClassName('create')[0];
+            getIframeContainerDocument().getElementsByClassName('hd')[0].insertBefore(avatarContainerElement, createDocumentElement);
+            // 清除头像框底部 border
+            setCssForIframeContainer('.sidebar .hd{box-shadow: none; border-bottom:none}');
 
+            // 设置 create top
+            setCssForIframeContainer('.sidebar .hd .create{top: 50px}');
+            // 设置鼠标样式
+            setCssForIframeContainer('img, a, i, span, div, li{cursor: default!important}');
+            // 侧边栏下移
+            getIframeContainerDocument().getElementsByClassName('sidebar-content')[0].style.top = "200px";
+            getIframeContainerDocument().getElementsByClassName('sidebar-content')[0].style.bottom = "26px";
+            // 移除 sidebar-ft
+            getIframeContainerDocument().getElementsByClassName('sidebar-ft')[0].style.display = "none";
 
-    }
-    setElementVisibilityById('iframe-container', true);
-    document.getElementById('loading-container').style.opacity = "0";
-    setElementVisibilityById('loading-container', false);
+            // 顶部留空
+            // 文件列表
+            setCssForIframeContainer('.list-hd{border-bottom: none!important; top: 13px}');
+            setCssForIframeContainer('.list-bd{top: 80px!important; border-top: 1px solid #e0e1e5;}');
+            // 内容区域
+            setCssForIframeContainer('.file-detail .hd{border-bottom: none!important; top: 13px}');
+            setCssForIframeContainer('.content-container{margin-top: 20px!important; border-top: 1px solid #e0e1e5;}');
+            // 设置 empty 时背景色
+            setCssForIframeContainer('drag-component .empty{background-color: #ffffff}');
+        }
+        setElementVisibilityById('iframe-container', true);
+        document.getElementById('loading-container').style.opacity = "0";
+        setElementVisibilityById('loading-container', false);
+    }, 1500);
 }
 
 /**
@@ -63,4 +100,20 @@ function setElementVisibilityById (id, visible) {
  */
 function getIframeContainerDocument () {
     return document.getElementById('iframe-container').contentWindow.document;
+}
+
+/**
+ * 对 iframe-container 设置css
+ * @param {string} rule css
+ */
+function setCssForIframeContainer (rule) {
+    getIframeContainerDocument().styleSheets[getIframeContainerDocument().styleSheets.length - 1].insertRule(rule, 0);
+}
+
+function setStyleFromComputedStyle (element) {
+    element.style.cssText = window.getComputedStyle(element, null).cssText;
+    let childrenLength = element.children.length;
+    for (let i = 0; i < childrenLength; i++) {
+        setStyleFromComputedStyle(element.children[i]);
+    }
 }
